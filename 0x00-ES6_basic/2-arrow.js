e52a4718a76f37ea2223e724caa216943fbc2b20
skylimit96 <a.sehlaoui96@gmail.com>
2024-03-04 21:07:20 +0000

    test

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,10 @@
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
-  };
+	const self = this;
+	this.addNeighborhood = (newNeighborhood) => {
+		self.sanFranciscoNeighborhoods.push(newNeighborhood);
+		return self.sanFranciscoNeighborhoods;
+	};
 }
 
