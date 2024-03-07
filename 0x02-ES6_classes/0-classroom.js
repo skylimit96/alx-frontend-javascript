5ded43cdffd36699dcfc04a04c3d9e08deeb1df5
skylimit96 <a.sehlaoui96@gmail.com>
2024-03-07 10:53:17 +0000

    readme and 1st task done

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents the classroom.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new ClassRoom.
+   * @attribute {Number} maxStudentsSize - max number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,1 @@
+This is the readme file of 0x02-ES6_classes
