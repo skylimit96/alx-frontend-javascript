e10b659403be8a69c05270489148f1d2f9cbef35
skylimit96 <a.sehlaoui96@gmail.com>
2024-03-13 17:00:45 +0000

    update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,11 @@
 /**
  * Retrieves a list of students.
- * author Amine SEHLAOUI <https://github.com/skylimit96>
- * returns {{id: Number, firstName: String, location: String}[]}
+ * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-      ];
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
